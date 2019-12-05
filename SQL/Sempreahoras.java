dcb13b51b32b0e0e97dcf6c5521ea613f3decf69
PedroMonteiro46 <up201503105@fe.up.pt>
2019-12-05 17:47:21 +0000

    Servidor a criar thread para cada conexao, gere event_id

--- SQL/Sempreahoras.java
+++ SQL/Sempreahoras.java
@@ -1,10 +1,7 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Sempreahoras {
 	
@@ -13,9 +10,8 @@ public class Sempreahoras {
 	public static void main(String[] args) {
 		
 		ServSQL sql = new ServSQL();
-		//sql.tableEventos();
-		//sql.tableUsers();
-		int event_id = sql.getEvent_id();
+		AtomicInteger event_id = new AtomicInteger(sql.getEvent_id());
+		sql.close();
         ServerSocket serverSocket = null;
         Socket socket = null;
         boolean doRun = true;
@@ -31,53 +27,7 @@ public class Sempreahoras {
             } catch (IOException e) {
                 System.out.println("I/O error: " + e);
             }
-            InputStream inp = null;
-            BufferedReader brinp = null;
-            DataOutputStream out = null;
-            try {
-                inp = socket.getInputStream();
-                brinp = new BufferedReader(new InputStreamReader(inp));
-                out = new DataOutputStream(socket.getOutputStream());
-            } catch (IOException e) {
-                return;
-            }
-            String inline;
-            String outline;
-            try {
-                inline = brinp.readLine();
-                System.out.print("recebi\n" + inline + "\n");
-                if (inline.contains("addevento")) {
-                	outline = sql.addEvento(inline, event_id);
-                	if (outline.equals( "" + event_id )) event_id++;
-                }
-                else if (inline.contains("delevento")) {
-                	outline = sql.delEvento(inline);
-                }
-                else if (inline.contains("updateevento")) {
-                	outline = sql.updateEvento(inline);
-                }
-                else if (inline.contains("getevento")) {
-                	outline = sql.getEventos(inline);
-                }
-                else if (inline.contains("adduser")) {
-                	outline = sql.addUser(inline);
-                }
-                else if (inline.contains("deluser")) {
-                	outline = sql.delUser(inline);
-                }
-                else if(inline.contains("fechar")) {
-                	outline = "FECHAR";
-                	doRun = false;
-                }
-                else outline = "Nenhum tipo funcao detetado";
-            	System.out.print("mandei\n" + outline + "\n");
-            	out.writeBytes(outline + "\n");
-                out.flush();
-                socket.close();
-                brinp.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            new ConnThread(socket, event_id).start();
         }
         try {
 			serverSocket.close();
@@ -87,4 +37,3 @@ public class Sempreahoras {
 		}
 	}
 }
-
